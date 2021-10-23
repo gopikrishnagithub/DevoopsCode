d8feb4d1c32690d3f99167f6f99e3002218f6458
gopikrishnagithub <gopikrishnagithub@gmail.com>
2021-10-23 17:07:41 +0530

    devoops commit

---
+++ src/main/java/com/devoops/DevoopsApplication.java
@@ -0,0 +1,22 @@
+package com.devoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoopsApplication.class, args);
+		System.out.println("Project started.....");
+	}
+	
+	@GetMapping("/devoops")
+	public String getName() {
+		return "hii hello welcome to spring boot ";
+	}
+
+}
